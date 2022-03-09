4c1a3034e7ab3c4f94a6104c11eaae8d358b18b9
seatwork <mophisoft@gmail.com>
2022-03-10 01:29:50 +0800

    Update exception.ts

--- exception.ts
+++ exception.ts
@@ -8,7 +8,7 @@ export class Exception extends Error {
 
     status: number;
 
-    public constructor(message: string, status?: number) {
+    constructor(message: string, status?: number) {
         super(message);
         this.status = !status || status < 400 || status > 511
             ? HttpStatus.INTERNAL_SERVER_ERROR : status;
