6ea2ef13dff4cc50ebbe59eb80552dea5d6b69ff
seatwork <mophisoft@gmail.com>
2022-03-10 22:31:44 +0800

    Update TinyEngine.ts

--- examples/plugins/TinyEngine.ts
+++ examples/plugins/TinyEngine.ts
@@ -3,8 +3,8 @@ import { Engine } from "../../mod.ts";
 @Engine("render")
 export class TinyEngine {
 
-    render() {
-        console.log("i am template engine")
+    render(tmpl: string, data: any) {
+        console.log("i am template engine", tmpl, data);
         const name = "tester";
         return `<h1>Hello, ${name} I am renderred by template engine</h1>`;
     }
