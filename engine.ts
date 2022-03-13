f2b9bc84873b40318e00765d8da949f153f1fc9e
seatwork <mophisoft@gmail.com>
2022-03-13 19:45:03 +0800

    Update engine.ts

--- engine.ts
+++ engine.ts
@@ -7,13 +7,19 @@ export class Engine {
   // Cache the compiled template functions
   #fnCache: Record<string, Function> = {};
 
+  /**
+   * Render method
+   * @param path template file relative path
+   * @param data
+   * @returns rendered html
+   */
   render(path: string, data: any = {}) {
     path = path.replace(/^\/+/, '');
     let fn = this.#fnCache[path];
 
     if (!fn) {
       const tmpl = textDecode(Deno.readFileSync(resolve(path)));
-      fn = dotts.template(tmpl, { argName: Object.keys(data) });
+      fn = dotts.template(tmpl, { argName: Object.keys(data) }); // ignores the default argName "it"
       this.#fnCache[path] = fn;
     }
     return fn(data);
