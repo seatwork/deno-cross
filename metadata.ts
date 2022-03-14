6fd46fdaf089afc82175693c81f07dc6e0127269
seatwork <mophisoft@gmail.com>
2022-03-15 03:17:37 +0800

    Update metadata.ts

--- metadata.ts
+++ metadata.ts
@@ -23,16 +23,13 @@ export class Metadata {
      * to trigger the decorators
      */
     static async loadClasses(): Promise<void> {
-        console.log("resolve()=", resolve())
-        console.log("resolve(.)=", resolve("."))
-        console.log("resolve(./)=", resolve("./"))
-        console.log("Deno.cwd()=", Deno.cwd())
+        for await (const entry of walk(resolve())) {
+            console.log("entry.path=", entry.path)
 
-        // for await (const entry of walk(resolve())) {
-        //     if (entry.isFile && (entry.name.endsWith('.ts') || entry.name.endsWith('.tsx'))) {
-        //         await import(entry.path);
-        //     }
-        // }
+            if (entry.isFile && (entry.name.endsWith('.ts') || entry.name.endsWith('.tsx'))) {
+                // await import(entry.path);
+            }
+        }
         // // Then parse all the decorators
         // this.#compose();
     }
